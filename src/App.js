0f8fab2fd7887a4fee84e330f9174882d4cdba1e
MaestroGacek <kohout.jan@hotmail.cz>
2024-12-03 12:27:22 +0100

    Upravy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { BrowserRouter, Route, Routes, HashRouter } from "react-router-dom";
 import './App.css'; 
-import './Home.js';
 
 function App() {
   return (
-    <HashRouter basename='/'>
-    <Routes>
-        <Route index element={<Home/>} />
-   
-    </Routes>
-    </HashRouter>
-   
+    <div className='App'></div>
+  
   );
 }
 

9471e1de18e2e25e498078cf978a1d9f951f80f2
MaestroGacek <kohout.jan@hotmail.cz>
2024-12-15 21:01:29 +0100

    Update color scheme to gold and fix background image path

--- src/stranky/Home.js
+++ src/stranky/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Pozadi from '../obrazky/Pozadi.jpeg'
+import '../styl/Home.css'
 function Home() {
   return (
     <div className='home'>
-      <div className='homeText'>
+      <div className='homeText' style={{backgroundImage: `url(${Pozadi})`}}>
         <h1>Libišské vánočky</h1>
         <p>Nejlepší pečivo na světě</p>
         <Link to={'/menu'}>
